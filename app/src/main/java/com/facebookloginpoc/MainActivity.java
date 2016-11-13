934b673f6621099d097459bc6236a771a01857bc
aalishan565 <aalishan565@gmail.com>
2016-11-13 23:21:46 +0530

    Initial commit

---
+++ app/src/main/java/com/facebookloginpoc/MainActivity.java
@@ -0,0 +1,84 @@
+package com.facebookloginpoc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTvStatus;
+    private LoginButton mBtnFbLogin;
+    CallbackManager callbackManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        mTvStatus = (TextView) findViewById(R.id.tv_status);
+        mBtnFbLogin = (LoginButton) findViewById(R.id.fb_login_btn);
+        //mBtnFbLogin.setPublishPermissions("public_profile");
+        mBtnFbLogin.setReadPermissions(Arrays.asList("public_profile, email, user_birthday"));
+        callbackManager = CallbackManager.Factory.create();
+        mBtnFbLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                mTvStatus.setText("login sucess" + loginResult.getAccessToken().getUserId() + "\n" + loginResult.getAccessToken().getToken());
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.d("profile", response.toString());
+                                try {
+                                    String name = object.getString("name");
+                                 //   String gender = object.getString("gender");
+                                 //   String email = object.getString("email");
+                                 //   Toast.makeText(MainActivity.this, "Welcome " + name + " " + gender + " " + email, Toast.LENGTH_LONG).show();
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                request.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, "Login cancelled", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(MainActivity.this, "Error occured", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
